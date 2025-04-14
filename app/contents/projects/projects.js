1274b1dbcb5fb36ea172cf6e8479a3952b797b4f
Ryoga-88 <hryoga8723@gmail.com>
2025-04-14 16:11:18 +0900

    mianにマージしました

--- app/contents/projects/projects.js
+++ app/contents/projects/projects.js
@@ -1,5 +1,29 @@
 const cardsData = [
   {
+    imageSrc: "/images/classnotebook.jpg",
+    title: "NebulaAI",
+    description:
+      "洗練されたデザインのAIチャットです。gpt-4-turbo,gpt-4,gpt-4o-mini-ttsを使用しており、言語生成と音声合成機能も搭載しています。",
+    date: "2025-04-10",
+    link: "https://ai-nebula.vercel.app/", // リンク先URLを追加
+  },
+  {
+    imageSrc: "/images/classnotebook.jpg",
+    title: "ClassNotebook",
+    description:
+      "一緒に考えれば、もっと解ける。Class Notebook は、学生同士が課題を共有し協力して解決することで、理解を深め新たな発見を促す学習支援ツールです。",
+    date: "2025-04-05",
+    link: "https://class-notebook.vercel.app/", // リンク先URLを追加
+  },
+  {
+    imageSrc: "/images/his-lab.jpg",
+    title: "人間情報システム研究室のwebサイト",
+    description:
+      "私の研究室の人間情報システム研究室に関する情報をまとめたwebサイトです。研究室のメンバーや研究内容、活動内容などを紹介しています。",
+    date: "2025-04-01",
+    link: "https://his-lab.vercel.app/", // リンク先URLを追加
+  },
+  {
     imageSrc: "/images/portal-mind.jpg",
     title: "Portal Mind",
     description:
